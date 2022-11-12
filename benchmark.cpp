d492e0ce48ea7d3bb00784e341b70075db2afb30
meganlew <meganlew4@gmail.com>
2022-11-12 13:32:51 -0800

    code

--- benchmark.cpp
+++ benchmark.cpp
@@ -104,7 +104,8 @@ int main(int argc, char** argv)
         std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
 
         std::chrono::duration<double> elapsed = end_time - start_time;
-        
+        std::cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
+
         // now invoke the cblas method to compute the matrix-vector multiplye
         reference_dgemv(n, Acopy, Xcopy, Ycopy);
 
