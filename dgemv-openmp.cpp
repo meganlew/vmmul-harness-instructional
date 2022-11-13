01afa7980265832c74598bd98e24213624d4b66f
meganlew <meganlew4@gmail.com>
2022-11-13 13:43:04 -0800

    code

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -24,13 +24,14 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you may want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
-   sum = 0.0;
+   double sum = 0.0;
    #pragma omp parallel for reduction (+:sum)
    for(int i = 0; i < n; i++){
-      sum += y[i];
+      sum = y[i];
       for(int j = 0; j < n; j++){
          sum += A[i*n+j] * x[j];
       }
+      y[i] = sum;
    }
 }
 
